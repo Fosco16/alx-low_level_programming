248362bed35cc0ac74417e397b7d52dab22746f7
fosco16 <onwunalichinonso7@gmail.com>
2022-10-26 08:36:25 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to _98 - update the value of n to 98
- * @n: input number
- * Return: n
+ * reset_to _98 - resets value to 98
+ *
+ * @n: pointer integer
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 void reset_to_98(int *n);
-
-
-
-
-
-
-
-int _putchar(char c);
-
 
 #endif
